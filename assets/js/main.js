dfff955bce6e567fe9e6f1ca9f763c1eed1f911f
John Benedict Lim <johnbenedictlim@Johns-MacBook-Pro.local>
2026-02-25 01:30:29 -0500

    Update landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,24 +215,7 @@
 					}).trigger('resize.ie-intro-fix');
 				}
 
-			// Hide intro on scroll (> small).
-				breakpoints.on('>small', function() {
-
-					$main.unscrollex();
-
-					$main.scrollex({
-						mode: 'bottom',
-						top: '25vh',
-						bottom: '-50vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
-
-				});
+		
 
 			// Hide intro on scroll (<= small).
 				breakpoints.on('<=small', function() {
